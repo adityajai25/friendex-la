ef6b82b75844ed7401dcfdae13c106d56f4c1450
adityajai25 <adityajai243@gmail.com>
2023-01-02 01:28:59 +0530

    Chat app setup

---
+++ client/main.js
@@ -0,0 +1,38 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat-container');
+
+let loadInterval;
+
+function loader(element){
+    element.textContent = '';
+    loadInterval = setInterval(() => {
+        element.textContent += '.';
+        if(element.textContent.length > 3) {
+          element.textContent = '';
+        }
+    },300);
+}
+
+function typeText(element, text) {
+    let i = 0;
+    let interval = setInterval(() => {
+      if(index < text.length){
+        element.innerHTML += text.charAt(index); 
+        index++;
+      } else { 
+        clearInterval(interval);
+      }
+    },20);
+} 
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return 'id-${timestamp}-${hexadecimalString}';
+}
+
