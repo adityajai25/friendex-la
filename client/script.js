6af9386bba4baf3740dd1f2f2888ff41b5aa8f15
adityajai25 <adityajai243@gmail.com>
2023-01-02 11:35:32 +0530

    Completed

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://friendex-la.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
